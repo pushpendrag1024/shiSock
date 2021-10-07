521ef85fe48b7004b8c198d07597083362618464
SHIKHAR YADAV <65106263+ShikharY10@users.noreply.github.com>
2021-10-08 01:59:24 +0530

    refactor-0.8

--- shiSock-0.4.0/shiSock-alpha-highlyUnstable/server.py
+++ shiSock-0.4.0/shiSock-alpha-highlyUnstable/server.py
@@ -4,43 +4,48 @@ import base64
 import pickle
 import threading
 import multiprocessing
-import queue
-from functools import lru_cache
-import cProfile, pstats, io
-import time
 
-def profile(func):
-    
-    def  inner(*args, **kwargs):
-        print(f"================================={func.__name__}================================")
-        pr = cProfile.Profile()
-        pr.enable()
-        retval = func(*args, **kwargs)
-        pr.disable()
-        s = io.StringIO()
-        sortby = "cumulative"
-        ps = pstats.Stats(pr, stream = s).sort_stats(sortby)
-        ps.print_stats()
-        print(s.getvalue())
-        return retval
+class serverWindows():
 
-    return inner
+    def __init__(self,secure = False, DSP_enable=False, file = None, debug = False, MTCL : bool = True, MPCL : bool = None):
 
-class MAIN():
+        self.__debug = debug
 
-    def __init__(self,address : str, port : int, listeners : int, debug : bool = False) -> object:
+        if MPCL and MTCL:
+            raise ValueError("both 'MPCL' abd 'MTCL' should not be set to True")
 
-        self.__debug : bool = debug
+        elif not MPCL and not MTCL:
+            raise ValueError("both 'MPCL' abd 'MTCL' should not be set to False")
 
-        self.__WRITABLE : list = []
-        self.__INPUTS : list = []
-        self.__OUTPUTS : list = []
-        self.__USERNAME_QUEUE : dict = {}
-        self.__CUSTOM__MESSAGE_HANDLER : dict = {}
-        self.__SENDER_QUEUE = {}
-        self.__SENDER_REQUEST : str = []
-        self.conClients : str = []
+        else:
+            self.__MPCL = MPCL
+            self.__MTCL = MTCL
 
+        if secure:
+            if not file:
+                raise TypeError("__init__() missing 1 required positional argument: 'file'")
+            else:
+                self.__secure = secure
+                self.__file_location = file
+
+        else:
+            self.__secure = secure
+
+        self.__READABLE = []
+        self.__WRITABLE = []
+        self.__INPUTS = []
+        self.__OUTPUTS = []
+        self.__MESSAGE_QUEUES = {}
+        self.__CUSTOM_CHANNEL = []
+        self.__CALLBACK_LOOP = []
+        self.__RECEIVING_MSG = []
+        self.__MESSAGE_HANDLER = []
+        self.__BYPASS_MSG  = []
+        self.__SENDER_QUEUE = []
+        self.conClients = []
+        
+    def SERVER(self, address : str = None, port : int = None, listeners : int = None):
+        
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.setblocking(False)
@@ -57,25 +62,55 @@ class MAIN():
             target = self.__server,
             args = ()
         )
-        server_thread.daemon = True
-        server_thread.start()
-        
-    def __remove_sock(self,sock):
-        if self.__debug:
-            print("User Disconnected")
-        if sock in self.__OUTPUTS:
-            self.__OUTPUTS.remove(sock)
-        if sock in self.__WRITABLE:
-            self.__WRITABLE.remove(sock)
-        self.__INPUTS.remove(sock)
-        sock.close()
-        username = self.__USERNAME_QUEUE[sock]
-        try:
-            self.conClients.remove(username)
-        except:
-            pass
 
-        del self.__USERNAME_QUEUE[sock]
+        receiver_thread = threading.Thread(
+            target=self.__handler,
+            args = (
+                self.__RECEIVING_MSG,
+                self.__BYPASS_MSG,
+                self.__CUSTOM_CHANNEL,
+                self.__MESSAGE_HANDLER
+            )
+        )
+
+        sender_thread = threading.Thread(
+            target=self.__sender,
+            args = (
+                self.__WRITABLE,
+                self.__MESSAGE_QUEUES,
+                self.__BYPASS_MSG,
+                self.__SENDER_QUEUE,
+            )
+            )
+
+        if self.__MTCL:
+            callback_loop_thread = threading.Thread(
+                target=self.__callback_loop,
+                args = (
+                    self.__CALLBACK_LOOP,
+                )
+            )
+        if self.__MTCL:
+            callback_loop_process = multiprocessing.Process(
+                target=self.__callback_loop,
+                args = (
+                    self.__CALLBACK_LOOP,
+                )
+            )
+
+        server_thread.daemon = True
+        receiver_thread.daemon = True
+        sender_thread.daemon = True
+
+        server_thread.start()
+        receiver_thread.start()
+        sender_thread.start()
+        if self.__MTCL:
+            callback_loop_thread.daemon = True
+            callback_loop_thread.start()
+        else:
+            callback_loop_thread.daemon = True
+            callback_loop_process.start()
 
     def __server(self):
         data_recv_len = []
@@ -89,7 +124,7 @@ class MAIN():
                     con,addr = r.accept()
                     con.setblocking(False)
                     self.__INPUTS.append(con)
-                    self.__USERNAME_QUEUE[con] = "no_data"
+                    self.__MESSAGE_QUEUES[con] = "no_data"
 
                 else:
                     ini = list(zip(*data_recv_len))
@@ -97,89 +132,126 @@ class MAIN():
                         try:
                             data_len = int(r.recv(32).decode().strip("-"))
                         except ConnectionResetError:
-                            self.__remove_sock(r)
+                            print("User Disconnected")
+                            if r in self.__OUTPUTS:
+                                self.__OUTPUTS.remove(r)
+                            if r in self.__WRITABLE:
+                                self.__WRITABLE.remove(r)
+                            self.__INPUTS.remove(r)
+                            r.close()
+                            del self.__MESSAGE_QUEUES[r]
                             continue
                         except:
-                            continue
+                            pass
 
                         if data_len:
                             data_recv_len.append([r,data_len])
                         else:
-                            self.__remove_sock(r)
+                            print("User Disconnected")
+                            if r in self.__OUTPUTS:
+                                self.__OUTPUTS.remove(r)
+                            self.__INPUTS.remove(r)
+                            if r in self.__WRITABLE:
+                                self.__WRITABLE.remove(r)
+                            r.close()
+                            del self.__MESSAGE_QUEUES[r]
                             continue
                     else:
                         INDEX = ini[0].index(r)
                         try:
                             recv_len = data_recv_len.pop(INDEX)[1]
                             data = r.recv(recv_len)
-
                             data = pickle.loads(base64.b64decode(data))
-                            # print(self.__USERNAME_QUEUE)
-                            
-                            try:
-                                if self.__USERNAME_QUEUE[r] == "no_data":
-                                    del self.__USERNAME_QUEUE[r]
-                                    self.__USERNAME_QUEUE[data.strip("0")] = r
-                                    self.conClients.append(data.strip("0"))
-                                    self.__SENDER_QUEUE[data.strip("0")] = queue.Queue()
-                                    if r not in self.__OUTPUTS:
-                                        self.__OUTPUTS.append(r)
-                            except KeyError:
-                                if data["channel"] == "DSP_MSG":
-                                    self.__SENDER_QUEUE[data["target_name"]].put(data)
-                                    self.__SENDER_REQUEST.append(data["target_name"])
-                                elif data["channel"] in self.__CUSTOM__MESSAGE_HANDLER:
-                                    self.__CUSTOM__MESSAGE_HANDLER[data["channel"]].put(data)
-
+                            if self.__MESSAGE_QUEUES[r] == "no_data":
+                                self.__MESSAGE_QUEUES[r] = data.strip("0")
+                                self.conClients.append(data.strip("0"))
+                                if r not in self.__OUTPUTS:
+                                    self.__OUTPUTS.append(r)
+                            else:
+                                self.__RECEIVING_MSG.append(data)
                                 if r not in self.__OUTPUTS:
                                     self.__OUTPUTS.append(r)
                             
                         except ConnectionResetError:
-                            self.__remove_sock(r)
+                            print("User Disconnected")
+                            if r in self.__OUTPUTS:
+                                self.__OUTPUTS.remove(r)
+                            self.__INPUTS.remove(r)
+                            if r in self.__WRITABLE:
+                                self.__WRITABLE.remove(r)
+                            r.close()
+                            del self.__MESSAGE_QUEUES[r]
                             continue
                         except EOFError:
                             pass
+            for w in writable:
+                if w not in self.__WRITABLE:
+                    self.__WRITABLE.append(w)
 
-            self.__sender(writable, self.__USERNAME_QUEUE)
-                    
             for e in exception:
-                self.__remove_sock(r)
+                self.__INPUTS.remove(e)
+                if e in self.__OUTPUTS:
+                    self.__OUTPUTS.remove(e)
+                e.close()
+                del self.__MESSAGE_QUEUES[e]
 
-    def __sender(self,__writable, usernameQueue):
+    def __handler(self,__receivingMsg, __bypassMsg,__customChannel,__messageHandler):
+        while True:
+            for i, _data_ in enumerate(__receivingMsg):
+                if _data_["channel"] == "DSP_MSG":
+                    __bypassMsg.append([_data_["target_name"],_data_])
+                    __receivingMsg.pop(i)
+                elif _data_["channel"] in __customChannel:
+                    __messageHandler.append(_data_)
+                    __receivingMsg.pop(i)
 
-        for username in self.__SENDER_REQUEST:
-            try:
-                con = usernameQueue[username]
-            except KeyError:
-                continue
+    def __sender(self,__writable, __messageQueue, __bypassMsg, __senderQueue ):
+        while True:
 
-            if con._closed and con.fileno() == -1:
-                __writable.remove(con)
-                continue
+            for s in __writable:
+                if s._closed and s.fileno() == -1:
+                    __writable.remove(s)
+                try:
+                    username = self.__MESSAGE_QUEUES[s]
+                except KeyError:
+                    pass
+                bypassMsg = list(zip(*__bypassMsg))
+                sender_q = list(zip(*__senderQueue))
 
-            try:
-                msg = self.__SENDER_QUEUE[username].get_nowait()
-                prepare_send = base64.b64encode(pickle.dumps(msg))
-                con.send(str(len(prepare_send)).center(16,"|").encode("utf-8"))
-                con.send(prepare_send)
-                self.__SENDER_REQUEST.remove(username)
-            except queue.Empty:
-                continue
-            except KeyError:
-                continue
+                if len(bypassMsg) > 0:
+                    if username in bypassMsg[0]:
+                        INDEX = bypassMsg[0].index(username)
+                        prepare_send = base64.b64encode(pickle.dumps(bypassMsg[1][INDEX]))
+                        s.send(str(len(prepare_send)).center(16,"|").encode("utf-8"))
+                        s.send(prepare_send)
+                        __bypassMsg.pop(INDEX)
+                        print("Message bypasses")
 
-    
+                if len(sender_q) > 0:
+                    if username in sender_q[0]:
+                        INDEX = sender_q[0].index(username)
+                        prepare_send = base64.b64encode(pickle.dumps(sender_q[1][INDEX]))
+                        s.send(str(len(prepare_send)).center(16,"|").encode("utf-8"))
+                        s.send(prepare_send)
+                        __senderQueue.pop(INDEX)
+
+
+    def __callback_loop(self,__callbackLst):
+        while True:
+            for i,func in enumerate(__callbackLst):
+                __callbackLst.pop(i)
+                func[0](*func[1])
 
     def CREATE_CHANNEL(self,channels : str = None, multiple : bool = False):
         if multiple:
             if type(channels) is type([]):
                 for channel in channels:
-                    if channel not in self.__CUSTOM__MESSAGE_HANDLER:
-                        self.__CUSTOM__MESSAGE_HANDLER[channel] = queue.Queue()
+                    if channel not in self.__CUSTOM_CHANNEL:
+                        self.__CUSTOM_CHANNEL.append(channel)
         else:
-            if channels not in self.__CUSTOM__MESSAGE_HANDLER:
-                self.__CUSTOM__MESSAGE_HANDLER[channels] = queue.Queue()
-
+            if channels not in self.__CUSTOM_CHANNEL:
+                self.__CUSTOM_CHANNEL.append(channels)
+        pass
 
     def SEND(self,target_name, channel : str = None, data = None):
         if not channel:
@@ -199,46 +271,39 @@ class MAIN():
                     "target_name" : target_name,
                     "data" : data
                 }
-                self.__SENDER_QUEUE[target_name].put(prepare_send)
-                self.__SENDER_REQUEST.append(target_name)
+                self.__SENDER_QUEUE.append([prepare_send["target_name"],prepare_send])
 
         else:
             raise TypeError(f"{type(data)} is not allowed as a sendable data type for 'data'")
 
-    # @profile
-    def LISTEN(self,channel : str = None, function : object = None, args = None):
-
+    def LISTEN(self,channel : str = None, function : object = None, ex_counter = None, args = None):
         if not channel:
             raise TypeError("LISTEN() missing 1 required positional argument: 'channel'")
-
-        if channel not in self.__CUSTOM__MESSAGE_HANDLER:
-            raise None
-
-        try:
-            p_data = self.__CUSTOM__MESSAGE_HANDLER[channel].get_nowait()
-            # print(p_data)
-            if not args:
-                # self.__CALLBACK_LOOP.append([function,[p_data]])
-                function(*[p_data])
-            else:
-                args = list(args)
-                args.insert(0,p_data)
-                # self.__CALLBACK_LOOP.append([function,[args]])
-                function(*[args])
-
-        except queue.Empty:
-            pass
-
-    # def __callback_loop(self,__callbackLst):
-    #     while True:
-    #         for i,func in enumerate(__callbackLst):
-    #             __callbackLst.pop(i)
-    #             func[0](*func[1])
-
+        else:
+            found = False
+            index = None
             
+            if channel in self.__CUSTOM_CHANNEL:
+                for i,d in enumerate(self.__MESSAGE_HANDLER):
+                    if d["channel"] == channel:
+                        found = True
+                        index = i
+                        break
+                if found:
+                    if not args:
+                        p_data = self.__MESSAGE_HANDLER.pop(index)
+                        self.__CALLBACK_LOOP.append([function,[p_data]])
+                    else:
+                        p_data = self.__MESSAGE_HANDLER.pop(index)
+                        args = list(args)
+                        args.insert(0,p_data)
+                        self.__CALLBACK_LOOP.append([function,[args]])
+
+
 class server():
-    def __init__(self,address : str, port : int, listeners : int, debug : bool = False):
-        __parent = MAIN(address, port, listeners, debug)
+    def __init__(self,secure = False, DSP_enable=False, file = None, debug = False, MTCL : bool = True, MPCL : bool = None):
+        __parent = serverWindows(secure,DSP_enable,file,debug,MTCL,MPCL)
+        self.SERVER = __parent.SERVER
         self.CREATE_CHANNEL = __parent.CREATE_CHANNEL
         self.LISTEN = __parent.LISTEN
         self.SEND = __parent.SEND
